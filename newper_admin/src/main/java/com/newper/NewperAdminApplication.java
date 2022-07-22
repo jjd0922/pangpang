f31c76c2ffc52dbd2ce2c6d9e83e43cb5172da61
kwon <abraham@iam-possible.co.kr>
2022-07-22 10:59:18 +0900

    admin setting

--- newper_admin/src/main/java/com/newper/NewperAdminApplication.java
+++ newper_admin/src/main/java/com/newper/NewperAdminApplication.java
@@ -1,9 +1,16 @@
 package com.newper;
 
+import com.newper.component.SessionInfo;
+import com.newper.exception.MsgException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,5 +19,22 @@ public class NewperAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NewperAdminApplication.class, args);
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvide(){
+
+        return new AuditorAware<String>() {
+            @Autowired
+            private SessionInfo sessionInfo;
+
+            @Override
+            public Optional<String> getCurrentAuditor() {
+                if(sessionInfo.getId()==null){
+                    throw new MsgException("세션이 만료되었습니다. 다시 로그인 해주세요.");
+                }
+                return Optional.of(sessionInfo.getId());
+            }
+        };
     }
 }
---
+++ newper_admin/src/main/java/com/newper/component/SessionInfo.java
@@ -0,0 +1,18 @@
+package com.newper.component;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Component
+@Getter
+@Setter
+/** 세션 정보 저장하는 class*/
+public class SessionInfo {
+
+    private String id;
+}
