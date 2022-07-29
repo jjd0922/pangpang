0dbef1a03e57612db8568a0fd7d2dc1e8c152479
nuri <jelly@iam-possible.co.kr>
2022-07-29 17:56:51 +0900

    user service

--- newper_admin/src/main/java/com/newper/service/UserService.java
+++ newper_admin/src/main/java/com/newper/service/UserService.java
@@ -2,7 +2,9 @@ package com.newper.service;
 
 
 import com.newper.dto.ParamMap;
+import com.newper.entity.Company;
 import com.newper.entity.User;
+import com.newper.repository.CompanyRepo;
 import com.newper.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +19,15 @@ public class UserService {
 
     @Autowired
     private final UserRepo userRepo;
-
+    private final CompanyRepo companyRepo;
 
     @Transactional
     public Integer userCreate(ParamMap paramMap) {
+        Integer comIdx = (Integer) paramMap.get("comIdx");
+      //  companyRepo.fin 어쩌고
+        //user.setcompany(comidx)
+        //paramMap.put(Company);
+
         User user = paramMap.mapParam(User.class);
         System.out.println("user: " + user);
 
