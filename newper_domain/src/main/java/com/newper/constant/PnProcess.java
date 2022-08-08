f44e5efcbf0c2807199f574310f09dca6687635a
kwon <abraham@iam-possible.co.kr>
2022-08-08 14:30:01 +0900

    ERD

---
+++ newper_domain/src/main/java/com/newper/constant/PnProcess.java
@@ -0,0 +1,17 @@
+package com.newper.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+/** PROCESS_NEED.PN_PROCESS 영업검수 결과 공정 진행 여부*/
+public enum PnProcess {
+
+     BEFORE("영업 검수전")
+    ,N("공정 미진행")
+    ,Y("공정 진행")
+    ;
+
+    private String option;
+}
