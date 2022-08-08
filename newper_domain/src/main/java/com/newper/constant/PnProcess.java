49121e7718de8cfec422aae4c4dbafab20b7f58e
kwon <abraham@iam-possible.co.kr>
2022-08-08 14:32:04 +0900

    enum

--- newper_domain/src/main/java/com/newper/constant/PnProcess.java
+++ newper_domain/src/main/java/com/newper/constant/PnProcess.java
@@ -7,8 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 /** PROCESS_NEED.PN_PROCESS 영업검수 결과 공정 진행 여부*/
 public enum PnProcess {
-
-     BEFORE("영업 검수전")
+    BEFORE("영업 검수전")
     ,N("공정 미진행")
     ,Y("공정 진행")
     ;
