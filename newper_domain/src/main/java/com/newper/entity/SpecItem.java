45edf7963049c23376782bb8e70e1f3b87b616f9
IAM-SANGWAN <leesw3027@iam-possible.co.kr>
2022-08-29 09:30:45 +0900

    스펙 엔티티 수정

--- newper_domain/src/main/java/com/newper/entity/SpecItem.java
+++ newper_domain/src/main/java/com/newper/entity/SpecItem.java
@@ -13,12 +13,14 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Builder
 public class SpecItem {
+    @Id
+    private Long speciIdx;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "SPECI_SPEC_IDX", referencedColumnName = "specIdx")
     private Spec spec;
 
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "SPECI_SPECL_IDX", referencedColumnName = "speclIdx")
     private SpecList specList;
 
