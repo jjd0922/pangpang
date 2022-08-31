4dd3b8f47e4c2c829f715afa72da775930629306
ljs <developer@iam-possible.co.kr>
2022-08-31 10:48:45 +0900

    erd

--- newper_admin/src/main/java/com/newper/controller/view/OrderController.java
+++ newper_admin/src/main/java/com/newper/controller/view/OrderController.java
@@ -2,6 +2,7 @@ package com.newper.controller.view;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,11 +17,18 @@ public class OrderController {
 
     /**통합주문관리*/
     @GetMapping("")
-    public ModelAndView category(){
+    public ModelAndView order(){
         ModelAndView mav = new ModelAndView("order/order");
 
         return mav;
     }
+
+    @GetMapping("detail")
+    public ModelAndView detail(){
+        ModelAndView mav = new ModelAndView("order/detail");
+        return mav;
+    }
+
 
 
 
