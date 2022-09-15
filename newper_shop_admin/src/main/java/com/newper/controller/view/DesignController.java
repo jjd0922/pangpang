f5b730762857f241859ab1d3dc58e082aacc50d3
IAM-SEONGMO <tkdrms968@iam-possible.co.kr>
2022-09-15 09:33:14 +0900

    ing

--- newper_shop_admin/src/main/java/com/newper/controller/view/DesignController.java
+++ newper_shop_admin/src/main/java/com/newper/controller/view/DesignController.java
@@ -4,6 +4,7 @@ import com.newper.repository.ShopRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,6 +21,21 @@ public class DesignController {
         ModelAndView mav = new ModelAndView("design/commonDesign");
 
         mav.addObject("shopList", shopRepo.findAll());
+
+        return mav;
+    }
+
+    /** 분양몰 디자인*/
+    @GetMapping(value = "pop/design/{shopIdx}")
+    public ModelAndView shopDesign(@PathVariable Integer shopIdx){
+        ModelAndView mav = new ModelAndView("design/design");
+
+        return mav;
+    }
+
+    @GetMapping(value = "pop/header/{shopIdx}")
+    public ModelAndView shopHeader(@PathVariable Integer shopIdx){
+        ModelAndView mav = new ModelAndView("design/header");
 
         return mav;
     }
