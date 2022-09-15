6ab5295ef353b26e9cd99a999da7c16157555c3c
nuri <jelly@iam-possible.co.kr>
2022-09-15 10:34:17 +0900

    sms관리 데이터테이블 오류 수정완료

--- newper_domain/src/main/java/com/newper/constant/ShType.java
+++ newper_domain/src/main/java/com/newper/constant/ShType.java
@@ -1,11 +1,12 @@
 package com.newper.constant;
 
+import com.newper.constant.basic.EnumOption;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum ShType {
+public enum ShType implements EnumOption {
 
     /**SMS*/
     SMS("SMS"),
