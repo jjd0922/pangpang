1c0ecd9f695b47f2c2cb65a6069a587e2587aec0
ljs <developer@iam-possible.co.kr>
2022-09-16 18:04:48 +0900

    delivery v1

--- newper_domain/src/main/java/com/newper/constant/OState.java
+++ newper_domain/src/main/java/com/newper/constant/OState.java
@@ -10,6 +10,14 @@ public enum OState implements EnumOption {
 
      BEFORE("입금대기")
     ,DONE("주문완료")
+    ,READY("상품준비중")
+    ,DELIVERY_REQ("배송요청")
+    ,INSTALL_REQ("설치요청")
+    ,ING("배송중")
+    ,DELIVERY_DONE("배송완료")
+    ,INSTALL_DONE("설치완료")
+    ,RETURN_REQ("반품요청")
+    ,RETURN_DONE("반품완료")
     ;
 
     private String option;
