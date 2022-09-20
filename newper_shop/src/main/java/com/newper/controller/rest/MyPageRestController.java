05ef036c8a512e291eaf3303ea6e616e12152ff7
IAM-SEONGMO <tkdrms968@iam-possible.co.kr>
2022-09-20 15:37:53 +0900

    마이쇼핑 > 나의 쇼핑내역 > 주문배송조회> 아래메뉴

--- newper_shop/src/main/java/com/newper/controller/rest/MyPageRestController.java
+++ newper_shop/src/main/java/com/newper/controller/rest/MyPageRestController.java
@@ -25,31 +25,39 @@ public class MyPageRestController {
     @PostMapping("orders.ajax")
     public ModelAndView orders(ParamMap paramMap){
         ModelAndView mav = new ModelAndView("myPage/myOrderProductList");
-
+        String orderMenuTitle = "";
         List<Shop> shopList = new ArrayList<>();
         if(!paramMap.containsKey("menu")){
             throw new MsgException("잘못된 접근입니다");
         }else if(paramMap.getString("menu").equals("orders")){
             Shop shop = shopRepo.findById(1).orElseThrow(()-> new MsgException("ㄷㄷㄷ"));
             shopList.add(shop);
+            orderMenuTitle = "주문 접수";
         }else if(paramMap.getString("menu").equals("payment")){
             shopList = shopRepo.findAll();
+            orderMenuTitle = "결제 완료";
         }else if(paramMap.getString("menu").equals("ready")){
             Shop shop = shopRepo.findById(2).orElseThrow(()-> new MsgException("ㄷㄷㄷ"));
             shopList.add(shop);
+            orderMenuTitle = "상품 준비중";
         }else if(paramMap.getString("menu").equals("delStart")){
             Shop shop = shopRepo.findById(3).orElseThrow(()-> new MsgException("ㄷㄷㄷ"));
             shopList.add(shop);
+            orderMenuTitle = "배송출발";
         }else if(paramMap.getString("menu").equals("delIng")){
             Shop shop = shopRepo.findById(4).orElseThrow(()-> new MsgException("ㄷㄷㄷ"));
             Shop shop2 = shopRepo.findById(3).orElseThrow(()-> new MsgException("www"));
             shopList.add(shop);
+            orderMenuTitle = "배송중";
         }else if(paramMap.getString("menu").equals("delComplete")){
+            orderMenuTitle = "배송완료";
         }else if(paramMap.getString("menu").equals("cancel")) {
+            orderMenuTitle = "취소/교환/반품";
         }else if(paramMap.getString("menu").equals("all")){
             shopList = shopRepo.findAll();
         }
         mav.addObject("shopList", shopList);
+        mav.addObject("orderMenuTitle", orderMenuTitle);
         return mav;
     }
 }
