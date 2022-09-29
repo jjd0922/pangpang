c9a7f5e2c18fc3e80d7745282f4c3d18c2fa6cab
kwon <abraham@iam-possible.co.kr>
2022-09-29 12:05:24 +0900

    주문결제 옵션 정렬

--- newper_domain/src/main/java/com/newper/constant/CtType.java
+++ newper_domain/src/main/java/com/newper/constant/CtType.java
@@ -9,23 +9,14 @@ import lombok.Getter;
 /**거래처 구분코드(거래처의 유형)*/
 public enum CtType implements EnumOption {
 
-    /**본사*/
     MAIN("본사")
-    /**매입처*/
     ,PURCHASE("매입처")
-    /**판매처*/
     ,SELL("판매처")
-    /**입점사*/
     ,STORE("입점사")
-    /**도색업체*/
     ,RECOATING("도색업체")
-    /**수리업체*/
     ,REPAIRING("수리업체")
-    /**배송업체*/
     ,DELIVERY("배송업체")
-    /**설치업체*/
     ,SETTING("설치업체")
-    /**기타*/
     ,ETC("기타")
     ;
 
