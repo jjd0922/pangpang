def98b18156d504ce563257cfb6dc9ece5749263
hyeonkyoung <hyeonkyoung@iam-possible.co.kr>
2022-09-30 15:25:49 +0900

    아이디찾기

--- newper_shop/src/main/java/com/newper/repository/CustomerRepo.java
+++ newper_shop/src/main/java/com/newper/repository/CustomerRepo.java
@@ -1,7 +1,6 @@
 package com.newper.repository;
 
 import com.newper.entity.Customer;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
@@ -12,6 +11,4 @@ public interface CustomerRepo extends JpaRepository<Customer, Long> {
 
     Customer findByCuNameAndCuPhone(String cuName, String cuPhone);
 
-    /** Ci값으로 회원 찾기 (현재는 임시로 di)*/
-    Customer findByCuDi(String cuDi);
 }
