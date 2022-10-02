df048ae671ec90f4fd29d6671549e594516283ec
Moon-Seong-Mo <tkdrms968@naver.com>
2022-10-03 04:16:25 +0900

    cate

--- newper_shop/src/main/java/com/newper/controller/rest/ProductRestController.java
+++ newper_shop/src/main/java/com/newper/controller/rest/ProductRestController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,5 +26,17 @@ public class ProductRestController {
 
 
         return returnMapList;
+    }
+    /** mainsection 카테고리 메인섹션 상품 조회 */
+    @PostMapping(value = "category/{msIdx}/{scateIdx}.ajax")
+    public ModelAndView recommendCategoryProduct(@PathVariable("msIdx") Long msIdx
+                    , @PathVariable("scateIdx") Integer scateIdx){
+        ModelAndView mav = new ModelAndView("mainSection/mainSection_product :: categoryProduct");
+        Map<String,Object> map = new HashMap<>();
+        map.put("msIdx", msIdx);
+        map.put("scateIdx", scateIdx);
+        List<Map<String,Object>> mainsectionscateProductList = shopProductMapper.selectscateMainSectionProductList(map);
+        mav.addObject("shopProductList", mainsectionscateProductList);
+        return mav;
     }
 }
