4996bc26790f84044a53c739ec116a989e7d9fe8
nuri <jelly@iam-possible.co.kr>
2022-10-05 18:38:57 +0900

    shop as 뉴퍼/다른거래처 완료 ( asState 추가 전)

--- newper_shop/src/main/java/com/newper/service/MypageService.java
+++ newper_shop/src/main/java/com/newper/service/MypageService.java
@@ -53,7 +53,10 @@ public class MypageService {
 
         try {
             String as_mail = paramMap.getString("AS_MAIL1");
-            String as_mail2 = paramMap.getString("AS_MAIL2");
+            String as_mail2 = paramMap.getString("AS_MAIL3");
+//            if(as_mail2==null){
+//                throw new MsgException("이메일을 정확히 입력해주세요");
+//            }
             as.setAsMail(as_mail + "@" + as_mail2);
         } catch (NumberFormatException nfe) {
             throw new MsgException("메일주소를 입력해주세요");
@@ -82,7 +85,6 @@ public class MypageService {
             as.setAsName(as_name);
         }
 
-//        as.setOrderGs(null);
         as.setDeliveryNum(null);
         as.setAsState(AsState.REQUEST);
         as.setAsDate(LocalDate.now());
@@ -116,58 +118,6 @@ public class MypageService {
 
         as.setAsFile(jsonObject);
 
-////
-////         파일확장자 확인
-//        String asFileExtension = FilenameUtils.getExtension(AS_FILE.getOriginalFilename());
-//
-//        if (!asFileExtension.equals("jpg") && !asFileExtension.equals("gif") && !asFileExtension.equals("png")) {
-//            throw new MsgException("사진은 png,gif,png 형식으로만 업로드 해주세요.");
-//
-//        }
-//
-//            String asFilePath = Common.uploadFilePath(AS_FILE, "mypage/as_image/", AdminBucket.SECRET);
-//
-//
-//            as.setAsFile(asFilePath);
-        //as.setAsFileName(AS_FILE.getOriginalFilename());
-//            as.setAsFile("");
-//
-/*        String[] idxs = paramMap.getString("AS_FILE").split(",");
-
-        int cnt =0;
-        for(int i=0; i<idxs.length; i++){
-            try {
-
-                String file = "";
-                String fileName = "";
-
-                AfterService afterService = paramMap.mapParam(AfterService.class);
-                if(AS_FILE.getSize()!=0){
-                    file = Common.uploadFilePath(aa, "mypage/as_image/", AdminBucket.SECRET);
-                    fileName= AS_FILE.getOriginalFilename();
-                }
-
-                JSONObject jsonObject = new JSONObject();
-                JSONArray jsonArray = new JSONArray();
-
-                //파일 추가시 파일 갯수만큼 반복문 적용
-                for(int y=0; y<1; y++){
-                    JSONArray jsonArray2 = new JSONArray();
-                    jsonArray2.add(file);
-                    jsonArray2.add(fileName);
-                    jsonArray.add(jsonArray2);
-                }
-                jsonObject.put("files",jsonArray);
-
-                as.setAsFile(jsonObject);
-
-                afterServiceRepo.save(as);
-                cnt++;
-            }catch (Exception e){
-                continue;
-            }
-            return cnt;
-        }*/
         afterServiceRepo.save(as);
         return as.getAsIdx();
 
