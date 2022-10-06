fc5a57e33f20349876fb6d0a5b19205dfe257b3f
hyeonkyoung <hyeonkyoung@iam-possible.co.kr>
2022-10-06 18:31:54 +0900

    myPage > review crud

--- newper_shop/src/main/java/com/newper/controller/rest/ReviewRestController.java
+++ newper_shop/src/main/java/com/newper/controller/rest/ReviewRestController.java
@@ -4,6 +4,7 @@ import com.newper.dto.ParamMap;
 import com.newper.dto.ReturnMap;
 import com.newper.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,25 @@ public class ReviewRestController {
     private final ReviewService reviewService;
 
     @PostMapping("save.ajax")
-    public ReturnMap spReviewSave(ParamMap paramMap, MultipartFile[] photos) {
+    public ReturnMap saveReview(ParamMap paramMap, MultipartFile[] photos) {
         ReturnMap rm = new ReturnMap();
         reviewService.saveReview(paramMap, photos);
+        rm.setMessage("리뷰 등록 완료");
+        return rm;
+    }
+
+    @PostMapping("update/{rIdx}.ajax")
+    public ReturnMap updateReview(@PathVariable("rIdx") Long rIdx, ParamMap paramMap, MultipartFile[] photos) {
+        ReturnMap rm = new ReturnMap();
+        reviewService.updateReview(rIdx, paramMap, photos);
+        rm.setMessage("리뷰 수정 완료");
+        return rm;
+    }
+
+    @PostMapping("delete/{rIdx}.ajax")
+    public ReturnMap deleteReview(@PathVariable("rIdx") Long rIdx) {
+        ReturnMap rm = new ReturnMap();
+        reviewService.deleteReview(rIdx);
         return rm;
     }
 }
--- newper_shop/src/main/java/com/newper/service/ReviewService.java
+++ newper_shop/src/main/java/com/newper/service/ReviewService.java
@@ -13,12 +13,14 @@ import com.newper.repository.OrdersRepo;
 import com.newper.repository.ReviewRepo;
 import com.newper.repository.ShopProductRepo;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -36,7 +38,10 @@ public class ReviewService {
     private final ShopProductRepo shopProductRepo;
     /**상품리뷰등록*/
     @Transactional
-    public void saveReview(ParamMap paramMap, MultipartFile[] mfs) {
+    public Review saveReview(ParamMap paramMap, MultipartFile[] mfs) {
+        if (mfs.length > 10) {
+            throw new MsgException("최대 10개의 이미지 등록이 가능합니다.");
+        }
         Review review = paramMap.mapParam(Review.class);
         Customer customer = customerRepo.getReferenceByCuId(shopSession.getId());
         if (customer == null) {
@@ -48,14 +53,54 @@ public class ReviewService {
         review.setOrders(ordersRepo.getReferenceById(Long.parseLong(map.get("O_IDX") + "")));
         review.setShopProduct(shopProductRepo.getReferenceById(Long.parseLong(map.get("SP_IDX")+"")));
 
-        if (mfs.length > 0) {
-            List<String> fileList = new ArrayList<>();
-            for (MultipartFile mf : mfs) {
-                String file = Common.uploadFilePath(mf, "shop_product/review/", AdminBucket.OPEN);
-                fileList.add(file);
+        List<String> fileList = new ArrayList<>();
+        String[] acceptArray = {"jpg","jpeg","png","gif"};
+        for (MultipartFile mf : mfs) {
+            if (mf.isEmpty()) { // 파일 없을 시 반복문 종료
+                break;
             }
-            review.setRJson(fileList);
+            String extension = FilenameUtils.getExtension(mf.getOriginalFilename());
+            boolean isAcceptable = Arrays.stream(acceptArray).anyMatch(element -> element.equals(extension));
+            if (!isAcceptable) {
+                throw new MsgException("jpg, gif, png 형식 파일만 등록할 수 있습니다.");
+            }
+            String file = Common.uploadFilePath(mf, "shop_product/review/", AdminBucket.OPEN);
+            fileList.add(file);
         }
+        review.setRJson(fileList);
         reviewRepo.save(review);
+
+        // 포인트 적립처리 필요
+        return review;
+    }
+
+    /**리뷰 수정*/
+    @Transactional
+    public void updateReview(Long rIdx, ParamMap paramMap, MultipartFile[] mfs) {
+        Review review = reviewRepo.findById(rIdx).orElseThrow(() -> new MsgException("존재하지 않는 리뷰입니다."));
+        Review newReview = paramMap.mapParam(Review.class);
+        review.updateReview(newReview);
+
+        List<String> rJson = review.getRJson();
+        String[] acceptArray = {"jpg","jpeg","png","gif"};
+        for (MultipartFile mf : mfs) {
+            if (mf.isEmpty()) { // 파일 없을 시 반복문 종료
+                break;
+            }
+            String extension = FilenameUtils.getExtension(mf.getOriginalFilename());
+            boolean isAcceptable = Arrays.stream(acceptArray).anyMatch(element -> element.equals(extension));
+            if (!isAcceptable) {
+                throw new MsgException("jpg, gif, png 형식 파일만 등록할 수 있습니다.");
+            }
+            String file = Common.uploadFilePath(mf, "shop_product/review/", AdminBucket.OPEN);
+            rJson.add(file);
+        }
+        review.setRJson(rJson);
+    }
+
+    /**리뷰 삭제*/
+    @Transactional
+    public void deleteReview(Long rIdx) {
+        reviewRepo.deleteById(rIdx);
     }
 }
