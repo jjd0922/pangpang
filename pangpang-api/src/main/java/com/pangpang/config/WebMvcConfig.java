db00cbf749f3c8cfbc9719dbc60fca64e8863065
jd <jd0922@naver.com>
2024-11-26 00:42:45 +0900

    CORS 허용

--- pangpang-api/src/main/java/com/pangpang/config/WebMvcConfig.java
+++ pangpang-api/src/main/java/com/pangpang/config/WebMvcConfig.java
@@ -8,19 +8,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
-import java.util.Map;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
 
     // 스프링 서버 전역적으로 CORS 설정
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                    .allowedOrigins("*") // 허용할 출처
+                    .allowedOriginPatterns("*") // 허용할 출처
                     .allowedMethods("GET", "POST") // 허용할 HTTP method
                     .allowCredentials(true); // 쿠키 인증 요청 허용
 
